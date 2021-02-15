7526743c138e88a8c02f863e1e7a12c551fa873c
vldmr17 <svma365@gmail.com>
2021-02-15 23:33:59 +0300

    Initial Commit

---
+++ RPO1Lab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPO1Lab
+//
+//  Created by admin on 15.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RPO1Lab/RPO1LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPO1LabApp.swift
+//  RPO1Lab
+//
+//  Created by admin on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct RPO1LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
